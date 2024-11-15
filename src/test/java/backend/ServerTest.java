f29d77fade3c72d2e3e01384a3ff4cc479ee31fe
GunnarJohansson <146834192+GunnarJohansson@users.noreply.github.com>
2024-11-15 14:16:03 +0100

    Added Javadoc to ClientHandler Server ServerTest

--- src/test/java/backend/ServerTest.java
+++ src/test/java/backend/ServerTest.java
@@ -6,19 +6,39 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This class test the functionality of the {@code Server} class.
+ * It verifies the singleton behaviour of the server instance and
+ * ensures proper management of chat channels
+ */
 class ServerTest {
+    //Server instance used for testing.
     private Server server;
+    //The port number used for testing.
     private int port;
+
+    /**
+     * Sets up the test environment by initializing the server instance
+     * and assigning a test port before each test case.
+     */
     @BeforeEach
     void setUp() {
         port = 1234;
         server = Server.createServerInstance(port);
     }
 
+    /**
+     * Cleans up after each test case.
+     */
     @AfterEach
     void tearDown() {
     }
 
+    /**
+     * Test {@code createServerInstance} method to verify that:
+     * The server instance is successfully created.
+     * The singleton pattern is maintained
+     */
     @Test
     void createServerInstance() {
         //Check that server is not Null
