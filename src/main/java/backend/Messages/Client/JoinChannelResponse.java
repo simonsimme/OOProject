5aa40a8321c38eee42fd1ebe16a9936e65aa482f
Alexander Lisborg <alexanderlisborg@gmail.com>
2024-11-22 10:15:12 +0100

    added comments to some message classes.

--- src/main/java/backend/Messages/Client/JoinChannelResponse.java
+++ src/main/java/backend/Messages/Client/JoinChannelResponse.java
@@ -1,9 +1,18 @@
 package backend.Messages.Client;
 
+/**
+ * Message to  client : Join the gi ven channel
+ */
 public class JoinChannelResponse extends ClientMessage {
+    /**
+     * This string represents the name of the channel that the user has joined.
+     */
     private String channelName;
 
-
+    /**
+     * Constructor.
+     * @param channelName The name of the channel that the user has joined.
+     */
     public JoinChannelResponse(String channelName){
         this.channelName = channelName;
     }
@@ -12,6 +21,10 @@ public class JoinChannelResponse extends ClientMessage {
         return channelName;
     }
 
+    /**
+     * Visitor pattern method.
+     * @param visitor
+     */
     @Override
     public void accept(ClientMessageVisitor visitor) {
 
