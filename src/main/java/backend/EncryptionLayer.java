7ccd29c2fce0261a9b5f3c457bc66b4bb4637aa5
yusnof <yusnofal@gmail.com>
2024-11-26 16:02:06 +0100

    More tests and more bug fixes in EncryptionLayer.java

--- src/main/java/backend/EncryptionLayer.java
+++ src/main/java/backend/EncryptionLayer.java
@@ -10,6 +10,13 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
+/**
+ * This class provides methods to encrypt and decrypt messages using the AES encryption algorithm. The idea of usage is to
+ * use the class staticly to generate a key, encrypt a message and decrypt a message.
+ * a solution to insure a layer of security is that we create a key per channel or per user.
+ * This way, if a key is compromised, only the messages of that channel or user are compromised.
+ */
+
 public class EncryptionLayer {
     /*
     AES is a symmetric encryption algorithm and a block cipher.
@@ -17,13 +24,28 @@ public class EncryptionLayer {
     The sender and the receiver must both know -- and use -- the same secret encryption key.
      */
     private static final String ALGORITHM = "AES";
+    private static final int keysize = 128;
 
     public static SecretKey generateKey() throws Exception {
         KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
-        keyGen.init(128);
+        keyGen.init(keysize);
         return keyGen.generateKey();
     }
 
+    public static String encrypt(Object encryptedData, SecretKey key) throws Exception {
+        if(encryptedData != null) {
+
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = cipher.doFinal(encryptedData.toString().getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);}
+
+        else {
+            throw new Exception("The message is null, you can't encrypt a null message");
+        }
+    }
+
+    //TODO first fix bugs but messages and then start implementing this method.
     public static String encrypt(Message message, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, key);
@@ -38,4 +60,17 @@ public class EncryptionLayer {
         byte[] decryptedBytes = cipher.doFinal(decodedBytes);
         return new String(decryptedBytes);
     }
+    public static String decrypt(Object encryptedData, SecretKey key) throws Exception {
+        if(encryptedData != null) {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedData.toString());
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+        return new String(decryptedBytes);
+        }
+        else {
+            throw new Exception("The message is null, you can't decrypt a null message");
+        }
+    }
+
 }
