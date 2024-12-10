a82a27e687067068106203a9b883454f93507a2e
yusnof <yusnofal@gmail.com>
2024-12-10 11:14:02 +0100

    write a string when creating txt file

--- src/main/java/Model/Server/saving/ChatSaver.java
+++ src/main/java/Model/Server/saving/ChatSaver.java
@@ -1,42 +1,45 @@
 package Model.Server.saving;
+
 import Model.Message;
 import Model.Server.ChatChannel;
 
 import java.io.*;
 
+/**
+ * The {@code ChatSaver} class is responsible for saving chat messages to a file.
+ */
 public class ChatSaver {
     private static String filePath;
-    //private final String folderPath;
     private BufferedWriter writer;
 
-    //private final String pathToLoger = "./src/main/java/Model/Server/saving/logs";
+    private final String startString;
+
 
     public ChatSaver(ChatChannel channel) {
         //dont remove the dot in the start
-        this("./src/main/java/Model/Server/saving/logs" , channel);
+        this("./src/main/java/Model/Server/saving/logs", channel);
     }
 
     private ChatSaver(String folderPath, ChatChannel channel) {
         this.filePath = folderPath + File.separator + channel.getName() + ".txt";
+        this.startString = "/*******Chat history for channel: " + channel.getName() + "********/";
         try {
             File folder = new File(folderPath);
             if (!folder.exists()) {
                 folder.mkdirs();
             }
             this.writer = new BufferedWriter(new FileWriter(filePath, true));
+            writer.write(startString);
         } catch (IOException e) {
             System.err.println("Error initializing writer: " + e.getMessage());
         }
     }
 
-    public void update(String channelName, Message message) {
-        saveMessage(channelName, message);
-    }
+
     public void saveMessage(String channelName, Message message) {
         try {
 
             writer.write(message.toString());
-
 
 
             writer.newLine();
@@ -47,6 +50,13 @@ public class ChatSaver {
             System.err.println("Error writing message to file: " + e.getMessage());
         }
     }
+
+    /**
+     * Reads the content of the file and returns it as a {@link StringBuilder}.
+     * This is statick method to be able to read the history of the channel without creating an instance of the class.
+     *
+     * @return
+     */
     private static StringBuilder readFile() {
         StringBuilder content = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
