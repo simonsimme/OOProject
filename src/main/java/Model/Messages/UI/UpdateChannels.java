02375e2e915207d97f4d3f88f5b404ada2d7e704
gunde <gunde_89@hotmail.com>
2024-12-15 13:15:38 +0100

    Added javadoc and fixed warnings.

--- src/main/java/Model/Messages/UI/UpdateChannels.java
+++ src/main/java/Model/Messages/UI/UpdateChannels.java
@@ -2,22 +2,52 @@ package Model.Messages.UI;
 
 import java.util.List;
 
+/**
+ * Message to update the list of channels and set the current channel.
+ * This class is used to notify the view about the available channels and the currently active channel.
+ */
 public class UpdateChannels extends UIMessage {
-    private List<String> channels;
-    private String current;
+    /**
+     * The list of channel names that are available to the user.
+     */
+    private final List<String> channels;
 
+    /**
+     * The name of the current channel the user is in.
+     */
+    private final String current;
+
+    /**
+     * Constructor to create an UpdateChannels message.
+     *
+     * @param channels A list of available channel names.
+     * @param current The name of the current channel the user is in.
+     */
     public UpdateChannels(List<String> channels,String current){
         this.channels = channels;
         this.current = current;
     }
 
+    /**
+     * Gets the list of available channels.
+     * @return A list of channel names.
+     */
     public List<String> getChannels() {
         return channels;
     }
+
+    /**
+     * Gets the name of the current channel the user is in.
+     * @return The name of the current channel.
+     */
     public String getCurrentChannel() {
         return current;
     }
 
+    /**
+     * Accepts a visitor to handle this UpdateChannels message.
+     * @param visitor The visitor that will handle the message.
+     */
     @Override
     public void accept(UIMessageVisitor visitor) {
         visitor.handle(this);
