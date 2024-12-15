99df935d50114bb27606f6c3d89287e481070832
gunde <gunde_89@hotmail.com>
2024-12-15 11:18:01 +0100

    Added javadoc to JoinChannelResponse and fixed warning.

--- src/main/java/Model/Messages/Client/JoinChannelResponse.java
+++ src/main/java/Model/Messages/Client/JoinChannelResponse.java
@@ -1,30 +1,34 @@
 package Model.Messages.Client;
 
 /**
- * Message to  client : Join the gi ven channel
+ * Represents a response message sent to a client, indicating that they have successfully joined a channel.
+ * This class extends {@code ClientMessage} and includes the name of the channel that the client joined.
  */
 public class JoinChannelResponse extends ClientMessage {
     /**
      * This string represents the name of the channel that the user has joined.
      */
-    private String channelName;
+    private final String channelName;
 
     /**
-     * Constructor.
+     * Constructs a {@code JoinChannelResponse} with the given channel name.
      * @param channelName The name of the channel that the user has joined.
      */
     public JoinChannelResponse(String channelName){
         this.channelName = channelName;
     }
 
-    //Getter
+    /**
+     * Gets the name of the channel that the user has joined.
+     * @return The name of the channel.
+     */
     public String getChannelName() {
         return channelName;
     }
 
     /**
-     * Visitor pattern method.
-     * @param visitor
+     * Accepts a visitor to handle this {@code JoinChannelResponse} message as part of the Visitor pattern.
+     * @param visitor The visitor that processes this message.
      */
     @Override
     public void accept(ClientMessageVisitor visitor) {
