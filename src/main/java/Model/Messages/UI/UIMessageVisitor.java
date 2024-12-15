d6a2787062bc4d60da6e57e86c7d01e603c5ffad
gunde <gunde_89@hotmail.com>
2024-12-15 13:11:23 +0100

    Added javadoc

--- src/main/java/Model/Messages/UI/UIMessageVisitor.java
+++ src/main/java/Model/Messages/UI/UIMessageVisitor.java
@@ -1,8 +1,35 @@
 package Model.Messages.UI;
 
+/**
+ * Visitor interface for handling different types of UI messages.
+ * This interface defines methods for handling specific message types that implement the {@link UIMessage} interface.
+ */
 public interface UIMessageVisitor {
+    /**
+     * Handles a {@link DisplayError} message.
+     * This method is called when a {@code DisplayError} message is encountered.
+     * @param e The {@code DisplayError} message to be handled.
+     */
     void handle(DisplayError e);
+    /**
+     * Handles a {@link DisplayMessage} message.
+     * This method is called when a {@code DisplayMessage} message is encountered.
+     *
+     * @param m The {@code DisplayMessage} to be handled.
+     */
     void handle(DisplayMessage m);
+    /**
+     * Handles an {@link UpdateChannels} message.
+     * This method is called when an {@code UpdateChannels} message is encountered.
+     *
+     * @param u The {@code UpdateChannels} message to be handled.
+     */
     void handle(UpdateChannels u);
+    /**
+     * Handles a {@link UIChannelHistory} message.
+     * This method is called when a {@code UIChannelHistory} message is encountered.
+     *
+     * @param historyMessage The {@code UIChannelHistory} message to be handled.
+     */
     void handle(UIChannelHistory historyMessage);
 }
