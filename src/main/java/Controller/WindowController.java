648255f0c4a782dea6753c577bf5a82f86ea0254
gunde <gunde_89@hotmail.com>
2024-12-15 14:14:57 +0100

    Added javadoc

--- src/main/java/Controller/WindowController.java
+++ src/main/java/Controller/WindowController.java
@@ -2,15 +2,26 @@ package Controller;
 
 import Model.Client.Client;
 import View.components.IView;
-
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+/**
+ * The {@code WindowController} class handles the window events, particularly the window closing event.
+ * It prompts the user with an exit confirmation dialog when the user attempts to close the application window.
+ * If the user confirms, it disconnects the client and closes the application.
+ */
 public class WindowController {
     private final IView view;
     private final Client reference;
 
+    /**
+     * Constructs a new {@code WindowController} to handle window events.
+     * This constructor sets up a listener for the window closing event.
+     *
+     * @param view     the view interface that represents the application's window
+     * @param reference the client instance associated with the user
+     */
     public WindowController(IView view, Client reference) {
         this.view = view;
         this.reference = reference;
@@ -22,6 +33,10 @@ public class WindowController {
         });
     }
 
+    /**
+     * Handles the window closing event by displaying a confirmation dialog to the user.
+     * If the user confirms the closure, the application will disconnect the client and close the window.
+     */
     private void handleWindowClosing() {
         int confirm = JOptionPane.showOptionDialog(
                 null,
