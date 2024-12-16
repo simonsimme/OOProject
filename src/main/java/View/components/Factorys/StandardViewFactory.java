3eb15daf6b81af88a6a28d3b336cf8f11dd7ae75
Simon <64027803+simonsimme@users.noreply.github.com>
2024-12-16 22:01:28 +0100

    @username will make it yellow, /color "texthere" will color the text

--- src/main/java/View/components/Factorys/StandardViewFactory.java
+++ src/main/java/View/components/Factorys/StandardViewFactory.java
@@ -1,15 +1,15 @@
 package View.components.Factorys;
 
 import View.components.Decoraters.EmojiDecorator;
-import View.components.Decoraters.TimestampDecorator;
+import View.components.Decoraters.MessageDecorator;
 import View.components.IView;
 import View.StandardView;
 
 public class StandardViewFactory implements ViewFactory {
 
-    //returns a new StandardView object with the TimestampDecorator and ErrormessageDecorater decorators
+    //returns a new StandardView object with the MessageDecorator and ErrormessageDecorater decorators
     @Override
     public IView createView() {
-        return (new EmojiDecorator(new TimestampDecorator(new StandardView())));
+        return (new EmojiDecorator(new MessageDecorator(new StandardView())));
     }
 }
