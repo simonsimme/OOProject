a1a05d2a72634141a58efa25fe703db2e6e7940c
yusnof <yusnofal@gmail.com>
2024-12-16 11:01:47 +0100

    updated the variables in ChaChannel

--- src/main/java/Model/Server/ChatChannel.java
+++ src/main/java/Model/Server/ChatChannel.java
@@ -15,9 +15,9 @@ import java.util.logging.Level;
  * Clients in the same chat channel can send messages to each other.
  */
 public class ChatChannel {
-    private final String name;
-    private final String password;
-    private final Set<ClientHandler> clients;
+    private final String ChannelName;
+    private final String ChannelPassword;
+    private final Set<ClientHandler> clientsSet;
     private final Set<SaveObserver> observers;
 
    ChatSaver chatSaverLocal;
@@ -28,9 +28,9 @@ public class ChatChannel {
      * @param password the password for the chat channel.
      */
     public ChatChannel(String name, String password) {
-        this.name = name;
-        this.password = password;
-        this.clients = new HashSet<>();
+        this.ChannelName = name;
+        this.ChannelPassword = password;
+        this.clientsSet = new HashSet<>();
         this.observers = new HashSet<>();
         this.chatSaverLocal = new ChatSaver(this);
         Server.logger.fine("Creating chat channel: " + name + " with password: " + password);
@@ -43,15 +43,15 @@ public class ChatChannel {
      */
     public synchronized boolean validatePassword(String password) {
 
-        return this.password.equals(password);
+        return this.ChannelPassword.equals(password);
     }
     /**
      * Adds a client to the chat channel.
      * @param client the client to add.
      */
     public synchronized void addClient(ClientHandler client) {
-        Server.logger.fine("Adding client: " + client.getName() + " to chat channel: " + this.name);
-        boolean added = clients.add(client);
+        Server.logger.fine("Adding client: " + client.getName() + " to chat channel: " + this.ChannelName);
+        boolean added = clientsSet.add(client);
         if(!added){
             //TODO throw exception or something like that to notify the client that he is already in the channel
             Server.logger.log(Level.SEVERE, "Client already in the channel or we have a problem in adding the client");
@@ -63,7 +63,7 @@ public class ChatChannel {
      * @param client the client to remove.
      */
     public synchronized void removeClient(ClientHandler client) {
-        boolean removed = clients.remove(client);
+        boolean removed = clientsSet.remove(client);
         if(!removed){
             //TODO: throw exception or something like that to notify the client that he is not in the channel
             Server.logger.log(Level.SEVERE, "Client not in the channel or we have a problem in removing the client");
@@ -75,14 +75,14 @@ public class ChatChannel {
      * @param message The message to be sent
      */
     public synchronized void broadcast(Message message) {
-        for (ClientHandler client : clients) {
+        for (ClientHandler client : clientsSet) {
                 client.sendMessage(message);
         }
         notifyObservers(message);
     }
     private void notifyObservers(Message message) {
         for (SaveObserver observer : observers) {
-            observer.update(name, message);
+            observer.update(ChannelName, message);
         }
     }
     //method to turn the string builder to later send to the client
@@ -96,11 +96,11 @@ public class ChatChannel {
      */
 
     public Set<ClientHandler> getClients() {
-        return clients;
+        return clientsSet;
     }
 
     public String getName() {
-        return name;
+        return ChannelName;
     }
 
     public void addObserver(ChatSaverObserver observer){
